79b7aef723980f8f1eb33c0e6f07901019360ef1
Alejandro Olchik <aolchik@ionatec.com.br>
2013-05-22 11:04:36 -0300

    working on sample test spies

--- src/test/java/com/aolchik/tdd/doubles/CalculatorDisplaySpyTest.java
+++ src/test/java/com/aolchik/tdd/doubles/CalculatorDisplaySpyTest.java
@@ -1,14 +1,29 @@
 package com.aolchik.tdd.doubles;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
+
+import org.junit.Before;
 import org.junit.Test;
+
+import com.aolchik.tdd.doubles.CalculatorDisplayStubTest.StubCalculator;
+import com.aolchik.tdd.simple.Calculator;
 
 public class CalculatorDisplaySpyTest {
 
+	@Before
+	public void setUp() {
+		this.calc = mock(Calculator.class);
+		this.display = new CalculatorDisplay((Calculator) calc);
+	}
+	
 	@Test
 	public void test() {
 		fail("Not yet implemented");
 	}
+
+	private CalculatorDisplay display;
+	private StubCalculator mockedCalc;
 
 }
