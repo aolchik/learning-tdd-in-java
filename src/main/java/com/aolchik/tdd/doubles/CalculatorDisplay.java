61c1ad365289b512d2371cc638f1e2be8f4130cd
Alejandro Olchik <aolchik@ionatec.com.br>
2013-05-22 16:15:00 -0300

    working with Decimal Formatter

--- src/main/java/com/aolchik/tdd/doubles/CalculatorDisplay.java
+++ src/main/java/com/aolchik/tdd/doubles/CalculatorDisplay.java
@@ -1,11 +1,20 @@
 package com.aolchik.tdd.doubles;
 
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+
 import com.aolchik.tdd.simple.Calculator;
 
 public class CalculatorDisplay {
 	
 	public CalculatorDisplay (Calculator calc) {
 		this.calculator = calc;
+		this.formatter = new DecimalFormat(DEFAULT_FORMATTING_PATTERN);
+		DecimalFormatSymbols dfs = new DecimalFormatSymbols();
+		decimalSeparator = DEFAULT_DECIMAL_SEPARATOR;
+		dfs.setDecimalSeparator(decimalSeparator.charAt(0));
+		//@TODO: throw exception when decimal separator is larger than one
+		this.formatter.setDecimalFormatSymbols(dfs);
 	}
 	
 	public void setSeparators(String thousands, String decimals) {
@@ -14,11 +23,15 @@ public class CalculatorDisplay {
 	}
 	
 	public String show() { 
-		return String.valueOf(calculator.result());
+		return formatter.format(calculator.result());
 	}
 
+	private static final String DEFAULT_FORMATTING_PATTERN = "#.00";
+	private static final String DEFAULT_DECIMAL_SEPARATOR = ".";
+	
 	private Calculator calculator;
 	private String thousandSeparator;
 	private String decimalSeparator;
+	private DecimalFormat formatter;
 	
 }
--- src/test/java/com/aolchik/tdd/doubles/CalculatorDisplaySpyTest.java
+++ src/test/java/com/aolchik/tdd/doubles/CalculatorDisplaySpyTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import com.aolchik.tdd.simple.Calculator;
@@ -15,8 +16,8 @@ public class CalculatorDisplaySpyTest {
 		// Arrange
 		Calculator mockedCalculator = mock(Calculator.class);
 		CalculatorDisplay display = new CalculatorDisplay(mockedCalculator);
-		double resultToInject = 100.0;
-		String expected = "100.0";
+		double resultToInject = 100;
+		String expected = "100.00";
 		when(mockedCalculator.result()).thenReturn(resultToInject);
 		
 		// Act
@@ -27,13 +28,63 @@ public class CalculatorDisplaySpyTest {
 		assertEquals("standard decimal separator", expected, actual);
 
 		// Code above was extracted to a method
-		verifyDisplayBehaviour("no standard thousand separator", 1000.0, "1000.0");
+		verifyDisplayBehaviour("no standard thousand separator", 1000, "1000.00");
 	}
 	
-	
+	@Test
+	public void showsAlternateSeparators() {
+		Calculator mockedCalculator = mock(Calculator.class);
+		CalculatorDisplay display = new CalculatorDisplay(mockedCalculator);		
+		String thousandSeparator = ".";
+		String decimalSeparator = ",";
+		display.setSeparators(thousandSeparator, decimalSeparator);
+
+		verifyDisplayBehaviour(display, mockedCalculator, "decimal separator change", 100, "100,0");
+		
+//		calc.enter(1000.0);
+//		assertEquals("single thousand separator", "1.000,0", display.show());
+//
+//		calc.enter(1000000000.0);
+//		assertEquals("multiple thousand separators", "1.000.000.000,0", 
+//				display.show());
+//
+//		calc.enter(-45000.0);
+//		assertEquals("thousand separator on negative numbers", "-45.000,0",
+//				display.show());
+	}
+
+	@Ignore
+	@Test
+	public void roundsBySpecificNumberOfDigits() {
+		fail("Not yet implemented");
+	}
+
+	@Ignore
+	@Test
+	public void showsHexadecimalRepresentation() {
+		fail("Not yet implemented");
+	}
+
+	@Ignore
+	@Test
+	public void showsBinaryRepresentation() {
+		fail("Not yet implemented");
+	}
+
+	@Ignore
+	@Test
+	public void showsScientificRepresentation() {
+		fail("Not yet implemented");
+	}
+
 	private void verifyDisplayBehaviour(String behaviour, double resultToInject, String expectedResult) {
 		Calculator mockedCalculator = mock(Calculator.class);
 		CalculatorDisplay display = new CalculatorDisplay(mockedCalculator);
+		
+		verifyDisplayBehaviour(display, mockedCalculator, behaviour, resultToInject, expectedResult);
+	}
+	
+	private void verifyDisplayBehaviour(CalculatorDisplay display, Calculator mockedCalculator, String behaviour, double resultToInject, String expectedResult) {	
 		when(mockedCalculator.result()).thenReturn(resultToInject);
 		
 		String actual = display.show();		
