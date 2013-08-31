2ccdf18d7f518eb59dd36323e4802f2cc6f00f34
aolchik <aolchik@ionatec.com.br>
2013-08-31 09:26:21 -0300

    Adding Fibonacci exercise.

---
+++ src/test/java/com/aolchik/tdd/simple/FibonacciTest.java
@@ -0,0 +1,16 @@
+package com.aolchik.tdd.simple;
+
+import static org.junit.Assert.*;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class FibonacciTest {
+
+	@Ignore
+	@Test
+	public void test() {
+		fail("pending implementation");
+	}
+
+}
